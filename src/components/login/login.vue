0878ad7ed5354310ea5b743a60598090bfa8b953
shi420068224 <42172518+shi420068224@users.noreply.github.com>
2020-06-25 18:27:37 +0800

    登录组件交互，结果处理

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,90 @@
 <template>
-  <div>LOGIN</div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formdata"
+    >
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="primary" @click.prevent="handleLogin()"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      labelPosition: "top",
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    //登录请求
 
-}
+    handleLogin() {
+      this.$http
+        .post("login", this.formdata)
+        .then(res => {
+          console.log(res);
+
+          const {
+            data,
+            meta: { msg, status }
+          } = res.data;
+
+          if (status === 200) {
+            /*
+              登陆成功
+                1、提示登录成功
+                2、跳转首页
+             */
+            this.$message.success(msg);
+            // this.$router.push({name: 'home'})
+          } else {
+            /*
+              登陆不成功
+                1、不成功提示
+            */
+            this.$message.error(msg);
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
+};
 </script>
 
 <style>
-
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  /* 弹性盒子布局 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrap .login-form {
+  width: 400px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import MyServerHttp from '@/plugins/http.js'
+
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/reset.css'
 import App from './App'
 import router from './router'
 
@@ -10,12 +13,15 @@ import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(MyServerHttp)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   // render: (h) => h(App)
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
