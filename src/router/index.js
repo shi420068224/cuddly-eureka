f75877da8824a9b24cd00c94530e301d60968905
shi420068224 <42172518+shi420068224@users.noreply.github.com>
2020-06-25 15:55:30 +0800

    新建登录分支，配置登录组件和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Login from '../components/login/login'
+// @ -> src/
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: '',
-      component: ''
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
