8341fce94ef4afa76e5bf32682a713d53f6b5aac
shi420068224 <42172518+shi420068224@users.noreply.github.com>
2020-06-26 11:39:47 +0800

    '状态和操作调整'

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -13,18 +13,45 @@
         <el-button type="info" plain>添加</el-button>
       </el-col>
     </el-row>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="userlist" style="width: 100%">
       <el-table-column label="#" width="60" type="index"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="140"> </el-table-column>
-      <el-table-column prop="address" label="邮箱" width="200">
+      <el-table-column prop="username" label="姓名" width="140">
       </el-table-column>
-      <el-table-column prop="address" label="电话" width="200">
+      <el-table-column prop="email" label="邮箱" width="200"> </el-table-column>
+      <el-table-column prop="mobile" label="电话" width="200">
       </el-table-column>
-      <el-table-column prop="address" label="创建时间" width="200">
+      <!-- <el-table-column prop="create_time" label="创建时间" width="200"></el-table-column> -->
+      <el-table-column label="创建时间" width="200">
+        <!--
+          若该单元格内显示的内容不是字符串，
+          需要给被显示的内容外层包裹一个template标签
+
+          template内部要使用数据，设置slot-scope属性
+          该属性的值是要用数据create_time的数据源 ->userlist
+
+          slot-scope的值userlist其实就是el-table绑定的数据userlist
+          userlist.row -> 数组中的每个对象
+         -->
+        <template slot-scope="scope">{{
+          scope.row.create_time | fmdate
+        }}</template>
       </el-table-column>
-      <el-table-column prop="address" label="用户状态" width="100">
+      <el-table-column label="用户状态" width="100">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          >
+          </el-switch>
+        </template>
       </el-table-column>
-      <el-table-column prop="address" label="操作" width="180">
+      <el-table-column prop="id" label="操作" width="180">
+        <template slot-scope="">
+          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+        </template>
       </el-table-column>
     </el-table>
   </el-card>
@@ -35,20 +62,11 @@ export default {
   data() {
     return {
       query: "",
+      userlist: [],
+      // 分页相关数据
+      total: -1,
       pagenum: 1,
-      pagesize: 2,
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        }
-      ]
+      pagesize: 2
     };
   },
   created() {
@@ -67,14 +85,15 @@ export default {
       const res = await this.$http.get(
         `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
       );
-
+      console.log(res);
       const {
-        data,
+        data: { users, total },
         meta: { msg, status }
       } = res.data;
       if (status === 200) {
-        console.log(res);
-        this.$message.success(msg)
+        this.userlist = users;
+        this.total = total;
+        this.$message.success(msg);
       } else {
         this.$message.error(msg);
       }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import MyServerHttp from '@/plugins/http.js'
+import moment from 'moment'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
@@ -14,6 +15,12 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 Vue.use(MyServerHttp)
+
+
+// 全局过滤器 - 处理日期
+Vue.filter('fmdate', (v) => {
+  return moment(v).format('YYYY-MM-DD')
+})
 
 /* eslint-disable no-new */
 new Vue({
