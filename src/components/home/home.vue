8ee0fd69a87c46e4e0f285d438dcad8b0a57e885
shi420068224 <42172518+shi420068224@users.noreply.github.com>
2020-07-01 01:07:45 +0800

    完善，路由守卫token认证显示组件;根据不同用户不同角色的权限，生成动态侧边栏导航

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -24,70 +24,22 @@
           text-color="#FFF"
         >
           <!-- 1 -->
-          <el-submenu index="1">
+          <el-submenu
+            :index="''+item1.order"
+            v-for="(item1, index) in menus"
+            :key="index"
+          >
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{ item1.authName }}</span>
             </template>
-            <el-menu-item index="users">
+            <el-menu-item
+              :index="item2.path"
+              v-for="(item2, index) in item1.children"
+              :key="index"
+            >
               <i class="el-icon-menu"></i>
-              <span>用户列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <!-- 2 -->
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="roles">
-              <i class="el-icon-location"></i>
-              <span>角色列表</span>
-            </el-menu-item>
-            <el-menu-item index="rights">
-              <i class="el-icon-location"></i>
-              <span>权限列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <!-- 3 -->
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="3-1">
-              <i class="el-icon-location"></i>
-              <span>商品列表</span>
-            </el-menu-item>
-            <el-menu-item index="3-1">
-              <i class="el-icon-location"></i>
-              <span>分类参数</span>
-            </el-menu-item>
-            <el-menu-item index="3-1">
-              <i class="el-icon-location"></i>
-              <span>商品分类</span>
-            </el-menu-item>
-          </el-submenu>
-          <!-- 4 -->
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="4-1">
-              <i class="el-icon-location"></i>
-              <span>订单列表</span>
-            </el-menu-item>
-          </el-submenu>
-          <!-- 5 -->
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="5-1">
-              <i class="el-icon-location"></i>
-              <span>数据报表</span>
+              <span>{{ item2.authName }}</span>
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -104,14 +56,34 @@ export default {
   //  获取tokan
   // if token 有 -> 继续渲染组件
   // token 没有 -> 登录
-
+  data() {
+    return {
+      menus: []
+    };
+  },
   beforeCreate() {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      this.$router.push({ name: "login" });
-    }
+    // const token = localStorage.getItem("token");
+    // if (!token) {
+    //   this.$router.push({ name: "login" });
+    // }
+    // 判断 token 的导航守卫/路由守卫 在router.js 新增
+  },
+  created() {
+    this.getMenus();
   },
   methods: {
+    async getMenus() {
+      const res = await this.$http.get(`menus`);
+      console.log(res);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.menus = data;
+        this.$message.info(msg);
+      }
+    },
     handleSignout() {
       // 1.清除token
       // 2.提示
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import {
+  Message
+} from 'element-ui';
+
 // import Login from '../components/login/login'
 // @ -> src/
 import Login from '@/components/login/login'
@@ -15,9 +20,8 @@ Router.prototype.push = function push(location) {
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
+const router = new Router({
+  routes: [{
       path: '/login',
       name: 'login',
       component: Login
@@ -26,8 +30,7 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
-      children: [
-        {
+      children: [{
           path: '/users',
           name: 'users',
           component: Users
@@ -46,3 +49,44 @@ export default new Router({
     }
   ]
 })
+
+// 导航守卫/路由守卫
+
+// 处理：统一判断是否存在token
+/*
+router.beforeEach((to,from,next)=>{
+
+})
+
+参数：
+  to    返回一个对象，去向 / 要去路由标识的配置信息
+  from  返回一个对象，来自 / 当前路由标识的配置信息
+  next  返回一个函数方法，next() / 继续执行
+*/
+
+router.beforeEach((to, from, next) => {
+  // 如果，去向 /login 页面，直接继续执行，不做任何处理
+  if (to.path === '/login') {
+    next()
+  } else {
+    const token = localStorage.getItem('token')
+    // 如果，没有token，去向 /login 页
+    // 提示请先登录
+    if (!token) {
+      Message.warning('请先登录')
+      router.push({
+        name: 'login'
+      })
+      return
+    }
+    // 如果，存在token，继续执行即可
+    next()
+  }
+})
+
+// this.$message.warning()，this指的是Vue实例
+// 在当前router.js配置文件中不存在Vue实例，根据ElementUI中，Message 消息提示
+// 可单独引用 Message 消息方法，import { Message } from 'element-ui';
+// 再调用，Message.warning()
+
+export default router
